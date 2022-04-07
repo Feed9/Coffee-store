69607b5693c6791b189b7333f83c44dcac7c6de9
Фёдор Суслов <youlox12345@yandex.ru>
2022-04-07 15:59:21 +0300

    Add PriceVolume CRUD

---
+++ Areas/Admin/Controllers/AdditionController.cs
@@ -0,0 +1,155 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Coffee_store.Data;
+using Coffee_store.Data.Entity;
+
+namespace Coffee_store.Areas.Admin
+{
+    [Area("Admin")]
+    public class AdditionController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdditionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Addition
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Additions.ToListAsync());
+        }
+
+        // GET: Admin/Addition/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var addition = await _context.Additions
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (addition == null)
+            {
+                return NotFound();
+            }
+
+            return View(addition);
+        }
+
+        // GET: Admin/Addition/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Addition/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Volume,Price")] Addition addition)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(addition);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(addition);
+        }
+
+        // GET: Admin/Addition/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var addition = await _context.Additions.FindAsync(id);
+            if (addition == null)
+            {
+                return NotFound();
+            }
+            return View(addition);
+        }
+
+        // POST: Admin/Addition/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Volume,Price")] Addition addition)
+        {
+            if (id != addition.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(addition);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AdditionExists(addition.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(addition);
+        }
+
+        // GET: Admin/Addition/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var addition = await _context.Additions
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (addition == null)
+            {
+                return NotFound();
+            }
+
+            return View(addition);
+        }
+
+        // POST: Admin/Addition/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var addition = await _context.Additions.FindAsync(id);
+            _context.Additions.Remove(addition);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AdditionExists(int id)
+        {
+            return _context.Additions.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Areas/Admin/Controllers/PriceVolumeController.cs
@@ -0,0 +1,160 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Coffee_store.Data;
+using Coffee_store.Data.Entity;
+
+namespace Coffee_store.Areas.Admin
+{
+    [Area("Admin")]
+    public class PriceVolumeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PriceVolumeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/PriceVolume
+        public async Task<IActionResult> Index(int productId)
+        {
+            List<PriceVolume> pricesVolumes = await _context.PricesVolumes.Where(pv => pv.ProductId == productId).ToListAsync();
+            int categoryId = Convert.ToInt32(_context.Products.FirstOrDefault(p => p.Id == productId).CategoryId);
+            ViewBag.Productid = productId;
+            ViewBag.Categoryid = categoryId;
+            return View(pricesVolumes);
+        }
+
+        // GET: Admin/PriceVolume/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var priceVolume = await _context.PricesVolumes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (priceVolume == null)
+            {
+                return NotFound();
+            }
+
+            return View(priceVolume);
+        }
+
+        // GET: Admin/PriceVolume/Create
+        public IActionResult Create(int productId)
+        {
+            PriceVolume priceVolume = new() { ProductId = productId };
+            return View(priceVolume);
+        }
+
+        // POST: Admin/PriceVolume/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ProductId,Price,Volume")] PriceVolume priceVolume)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(priceVolume);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index), new { ProductId = priceVolume.ProductId });
+            }
+            return View(priceVolume);
+        }
+
+        // GET: Admin/PriceVolume/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var priceVolume = await _context.PricesVolumes.FindAsync(id);
+            if (priceVolume == null)
+            {
+                return NotFound();
+            }
+            return View(priceVolume);
+        }
+
+        // POST: Admin/PriceVolume/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ProductId,Price,Volume")] PriceVolume priceVolume)
+        {
+            if (id != priceVolume.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(priceVolume);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PriceVolumeExists(priceVolume.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index), new { ProductId = priceVolume.ProductId });
+            }
+            return View(priceVolume);
+        }
+
+        // GET: Admin/PriceVolume/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var priceVolume = await _context.PricesVolumes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (priceVolume == null)
+            {
+                return NotFound();
+            }
+
+            return View(priceVolume);
+        }
+
+        // POST: Admin/PriceVolume/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var priceVolume = await _context.PricesVolumes.FindAsync(id);
+            _context.PricesVolumes.Remove(priceVolume);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index), new { ProductId = priceVolume.ProductId });
+        }
+
+        private bool PriceVolumeExists(int id)
+        {
+            return _context.PricesVolumes.Any(e => e.Id == id);
+        }
+    }
+}
