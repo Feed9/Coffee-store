b6d548b4f91c7151e044748e47c5fd63f7208193
Фёдор Суслов <youlox12345@yandex.ru>
2022-04-09 18:09:06 +0300

    add custom Authorization

---
+++ Controllers/UserAuthController.cs
@@ -0,0 +1,58 @@
+﻿using Coffee_store.Data;
+using Coffee_store.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Coffee_store.Controllers
+{
+    public class UserAuthController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+        public UserAuthController(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            Data.ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LogIn(UserLoginModel model)
+        {
+            model.LoginInValid = "true";
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+
+                if (result.Succeeded)
+                {
+                    model.LoginInValid = "";
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid Login attempt");
+                }
+            }
+            return PartialView("_UserLoginPartial", model);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LogOut(string? url = null)
+        {
+            await _signInManager.SignOutAsync();
+            if (url != null)
+            {
+                return LocalRedirect(url);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Models/UserLoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Coffee_store.Models
+{
+    public class UserLoginModel
+    {
+        [Required]
+        [StringLength(256),MinLength(5)]
+        [EmailAddress]
+        public string? Email { get; set; }
+        [Required]
+        [StringLength(256), MinLength(5)]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+        [Display(Name ="Remember me")]
+        public bool RememberMe { get; set; }
+        public string? LoginInValid { get; set; }
+    }
+}
