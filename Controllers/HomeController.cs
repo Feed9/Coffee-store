1c7278f5b683518a5236b980c993405f88e8b546
fedor <fedor@MORTUUSFLORES>
2022-06-14 11:12:31 +0300

    add design

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,22 +6,17 @@ namespace Coffee_store.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+            
         }
 
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        }       
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
